3a366abcb4e6b6e99910c54c99ba6271540d1cf8
Patrick_Santos <patrick.lloyd.santos@gmail.com>
2023-11-05 11:21:55 +0800

    Add project files.

---
+++ Azure-Web-App-Test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Azure_Web_App_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
